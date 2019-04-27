e7a6e507159f9416004cfe6756b9f75683137cb8
Chisatooo <donsimon5@icloud.com>
2019-04-27 12:45:56 +0900

    Initial Commit

---
+++ game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  game
+//
+//  Created by 西澤　茅里 on 2019/04/27.
+//  Copyright © 2019 litech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
